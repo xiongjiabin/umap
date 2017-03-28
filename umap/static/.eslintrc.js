22b57ce1bae7e9a20683f018677dc0a8c64fc23f
xiongjiabin <totty.xiong@gmail.com>
2017-03-28 14:46:05 +0800

    for eslint

---
+++ umap/static/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+    }
+};
